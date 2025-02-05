da67bf157e482bd68ebbedfad7d953bd159b6139
Gaurav Chaturvedi <2002gaurav.chaturvedi@gmail.com>
2025-02-06 01:17:23 +0530

    flask.py

--- flask.py
+++ flask.py
@@ -8,7 +8,7 @@ tfidf_vectorizer = pickle.load(open('tfidf_vectorizer.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('frontend.html')
 @app.route('/detect', methods=['POST'])
 def detect_plagiarism():
     input_text = request.form['text']
@@ -17,7 +17,7 @@ def detect_plagiarism():
     vectorized_text = tfidf_vectorizer.transform([input_text])
     result = model.predict(vectorized_text)
     result ="Plagiarism Detected" if result[0] == 1 else "No Plagiarism Detected"
-    return render_template('index.html', result=result)
+    return render_template('frontend.html', result=result)
 
 
 
